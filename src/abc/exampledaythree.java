ec8475643f5a04cb7ff41a73328b18672dc7fc00
mhali <m.halilyesil@gmail.com>
2020-02-03 20:00:51 -0600

    i started to code
    Signed-off-by: mhali <m.halilyesil@gmail.com>

---
+++ src/abc/exampledaythree.java
@@ -0,0 +1,41 @@
+package abc;
+
+public class exampledaythree {
+
+    public static void main(String [] args){
+/*
+        int a = 300;
+        byte b= (byte) a;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        double k = 2.123456789101112131415;
+        float l = (float) k;
+
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println((float)k);
+
+        String m = "Hello World";
+        char n = m.charAt(0);
+        System.out.println(n);
+
+
+- remainder- Arithmatic operators and various  operators for java
+
+*/
+
+System.out.println(7%2);
+System.out.println(9%4);
+
+int x=99;
+System.out.println(++x);
+System.out.println(--x);
+
+
+
+
+    }
+
+}
---
+++ src/abc/thirddayofjava.java
@@ -0,0 +1,72 @@
+package abc;
+import java.util.Scanner;
+public class thirddayofjava {
+
+    public static void main(String [] args){
+/*
+        Scanner myKeyboard = new Scanner(System.in);
+
+        System.out.print("how to be a man= ");
+        String isima = myKeyboard.nextLine();
+        System.out.print("so why are you here=");
+        String isimb = myKeyboard.nextLine();
+        System.out.print("are you feeling happy=");
+        String isimc = myKeyboard.nextLine();
+
+        System.out.println(isima+isimb+isimc);
+
+
+
+
+Java type conversion why?
+
+java certification circulm.
+
+2 requirements, they sould be compitable to conversion
+secod one is new assigned value should be smaller( from smaller container to large container) these are the aotomatic conversion ones.
+
+-they should be widening* thecnical terms must be known for the interwiews.
+
+
+type casting or narrowing, its depend of our dyta, it depends on the situaiton. we could lose the dyta or sth. we should chechk it.
+- Create double variabe cast to short(initialize(başlangıç durumuca getirmek) it) double myNum= 189.726
+
+- i should be android and swift developer i guess. i have to figure out sth. i had to search lots of things. i need routines.
+-
+
+
+    byte number1 = 99;
+    int number2 = number1;
+    int number3 = 128;
+    number1 = (byte) number3;
+    System.out.print(number1);
+
+
+        double number1 = 44.676;
+    short shortNum = (short)number1;
+    System.out.println(shortNum);
+
+*/
+    char myChar = 'a';
+    System.out.println((int)myChar);
+    int myInt = 99;
+    System.out.println((char)myInt);
+
+
+    //new example
+
+
+    int number1 = 10;
+    float number2 = 4.5f;
+    System.out.println(number1*number2);
+
+
+
+
+
+
+
+    }
+
+
+}
